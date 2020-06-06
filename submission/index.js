4eb1582fe303e4531445a22445f0cef92349b46b
akbar_haidarali <akbarhaidarali6@gmail.com>
2020-06-06 10:29:27 +0700

    ganti folder

--- submission/index.js
+++
@@ -1,8 +0,0 @@
-import covid from './src/data/indexprov.js';
-import covidDay from './src/data/indexday.js';
-import covidAllGender from './src/data/indexgender.js';
-import "./src/component/footer-app.js";
-
-covidDay();
-covid();
-covidAllGender();
